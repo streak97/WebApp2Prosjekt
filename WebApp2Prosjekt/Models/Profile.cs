f8f58446da936790925dfd8459dd362dd6b6ff2f
jov009 <jov009@post.uit.no>
2018-11-12 12:43:18 +0100

    Profile entity

---
+++ WebApp2Prosjekt/Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp2Prosjekt.Models
+{
+    public class Profile
+    {
+        public int ProfileId { get; set; }
+
+        public int LinesWritten { get; set; }
+
+        public decimal WagePerLine { get; set; }
+
+        public virtual IdentityUser Owner { get; set; }
+
+    }
+}
