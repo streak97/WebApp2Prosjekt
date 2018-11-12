4ba5ffa09a367dc3499f71b9f609575f06c19eff
jov009 <jov009@post.uit.no>
2018-11-12 10:39:34 +0100

    Setting up controllers and task entity

--- WebApp2Prosjekt/Data/Migrations/20181112092548_Ventures.Designer.cs
+++ WebApp2Prosjekt/Data/Migrations/20181112092548_Ventures.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp2Prosjekt.Data;
 
 namespace WebApp2Prosjekt.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181112092548_Ventures")]
+    partial class Ventures
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebApp2Prosjekt/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp2Prosjekt.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Complete { get; set; }
+
+        public int Lines { get; set; }
+
+        public virtual IdentityUser Client { get; set; }
+
+        public virtual IdentityUser Freelancer { get; set; }
+
+    }
+}
