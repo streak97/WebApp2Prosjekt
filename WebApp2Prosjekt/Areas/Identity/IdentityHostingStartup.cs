b0c6f49797fe96c9b3a98a6e5007c2d5677e0ae7
jov009 <jov009@post.uit.no>
2018-11-13 12:25:19 +0100

    Added scaffold for registration

---
+++ WebApp2Prosjekt/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp2Prosjekt.Data;
+
+[assembly: HostingStartup(typeof(WebApp2Prosjekt.Areas.Identity.IdentityHostingStartup))]
+namespace WebApp2Prosjekt.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
