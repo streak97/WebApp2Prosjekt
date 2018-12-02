8e2285af7f5db70330448246a2b9831152d55bbb
Terje Solend Nomeland <tjnome@gmail.com>
2018-12-02 14:34:23 +0100

    Fixed Frameworks

--- WebApp2Prosjekt/Repositories/DeveloperRepository.cs
+++ WebApp2Prosjekt/Repositories/DeveloperRepository.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApp2Prosjekt.Data;
