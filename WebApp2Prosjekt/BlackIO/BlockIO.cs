20ea05fb035ef376307ba6e990db69469d084c3a
Terje Solend Nomeland <tjnome@gmail.com>
2018-12-02 21:45:59 +0100

    Added BlockIO to #main

---
+++ WebApp2Prosjekt/BlackIO/BlockIO.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
+
+namespace WebApp2Prosjekt.BlackIO
+{
+
+    public class BlockIO
+    {
+        private string ApiKey { get; set; }
+        private string Url = "https://block.io/api/v2/";
+        private string Status;
+        private Dictionary<string, object> Data;
+
+        public BlockIO(string apiKey)
+        {
+            this.ApiKey = apiKey;
+        }
+
+        private JSONAPI wallet_API(string method, NameValueCollection parameters)
+        {
+            WebClient Client = new WebClient();
+            Uri url = new Uri(Url + method + "/");
+            JSONAPI json = new JSONAPI();
+
+
+            string JSONString = string.Empty;
+            parameters.Add("api_key", this.ApiKey);
+
+            try
+            {
+                JSONString = Client.DownloadString(url + "?" + parameters.ToString());
+            }
+            catch (WebException ex)
+            {
+                JSONString = new StreamReader(ex.Response.GetResponseStream()).ReadToEnd();
+            }
+
+            json = JsonConvert.DeserializeObject<JSONAPI>(JSONString);
+            if (json.Status == "success")
+            {
+                if (json.Data.ContainsKey("user_id"))
+                {
+                    json.Data["user_id"] = Convert.ToInt32(json.Data["user_id"]);
+                }
+            }
+            else
+            {
+                throw new Exception("Block.io API Error: " + json.Data["error_message"]);
+            }
+
+            return json;
+        }
+
+        public JSONAPI getNewAddress()
+        {
+            NameValueCollection Params = System.Web.HttpUtility.ParseQueryString(string.Empty);
+            return wallet_API("get_new_address", Params);
+        }
+
+        public JSONAPI getBalance()
+        {
+            NameValueCollection Params = System.Web.HttpUtility.ParseQueryString(string.Empty);
+            return wallet_API("get_balance", Params);
+        }
+
+        public JSONAPI getMyAddresses()
+        {
+            NameValueCollection Params = System.Web.HttpUtility.ParseQueryString(string.Empty);
+            return wallet_API("get_my_addresses", Params);
+        }
+
+        public JSONAPI getCurrentPrices()
+        {
+            NameValueCollection Params = System.Web.HttpUtility.ParseQueryString(string.Empty);
+            return wallet_API("get_current_prices", Params);
+        }
+
+        public JSONAPI getTransactions(string type)
+        {
+            NameValueCollection Params = System.Web.HttpUtility.ParseQueryString(string.Empty);
+            Params.Add("type", type);
+            return wallet_API("get_transactions", Params);
+        }
+
+        public JSONAPI withdraw(string amounts, string addresses, string pin)
+        {
+            NameValueCollection Params = System.Web.HttpUtility.ParseQueryString(string.Empty);
+
+            Params.Add("amounts", amounts);
+            Params.Add("to_addresses", addresses);
+            Params.Add("pin", pin);
+
+            return wallet_API("withdraw", Params);
+        }
+    }
+}
---
+++ WebApp2Prosjekt/BlackIO/JSONAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp2Prosjekt.BlackIO
+{
+    public class JSONAPI
+    {
+        public string Status;
+        public Dictionary<string, object> Data;
+    }
+}
