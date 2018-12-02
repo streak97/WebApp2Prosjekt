fe405b6b9382a47e0bbf8fcb09ac339197994b2f
Terje Solend Nomeland <tjnome@gmail.com>
2018-12-02 22:18:06 +0100

    Added PayForTaskViewModel

---
+++ WebApp2Prosjekt/Models/ViewModels/PayForTaskViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApp2Prosjekt.Models.ViewModels
+{
+    /// <summary>
+    /// ViewModel for creating a new task
+    /// </summary>
+    public class PayForTaskViewModel
+    {
+        public int TasksId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Complete { get; set; }
+
+        public int Lines { get; set; }
+    }
+}
