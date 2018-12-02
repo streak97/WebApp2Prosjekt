eedbb17269036ead1b38b5ad338114d59a861d84
Terje Solend Nomeland <tjnome@gmail.com>
2018-12-02 22:17:56 +0100

    Removed unused imports

--- WebApp2Prosjekt/Models/ViewModels/EditTaskViewModel.cs
+++ WebApp2Prosjekt/Models/ViewModels/EditTaskViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApp2Prosjekt.Models.ViewModels
+﻿namespace WebApp2Prosjekt.Models.ViewModels
 {
     public class EditTaskViewModel
     {
