98500b564e711173575d6edafb5ae2d98af3c932
Terje Solend Nomeland <tjnome@gmail.com>
2018-12-02 21:46:24 +0100

    Added Test for CoinBase and BlockIO

---
+++ ControllerTest/BlockIOTest.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApp2Prosjekt.BlackIO;
+
+namespace BlockIOTesting
+{
+    [TestClass]
+    public class BlockIOTest
+    {
+
+        [TestMethod]
+        public void can_getNewAddress()
+        {
+            string apiKey = "bb7c-fc4a-42a2-cf6e";
+            BlockIO blockio = new BlockIO(apiKey);
+
+            JSONAPI json = blockio.getNewAddress();
+
+            Assert.AreEqual("success", json.Status);
+        }
+
+        [TestMethod]
+        public void can_getBalance()
+        {
+            string apiKey = "bb7c-fc4a-42a2-cf6e";
+            BlockIO blockio = new BlockIO(apiKey);
+
+            JSONAPI json = blockio.getBalance();
+
+            Assert.AreEqual("success", json.Status);
+        }
+
+        [TestMethod]
+        public void can_getMyAddresses()
+        {
+            string apiKey = "bb7c-fc4a-42a2-cf6e";
+            BlockIO blockio = new BlockIO(apiKey);
+
+            JSONAPI json = blockio.getMyAddresses();
+
+            Assert.AreEqual("success", json.Status);
+        }
+
+        [TestMethod]
+        public void can_getCurrentPrices()
+        {
+            string apiKey = "bb7c-fc4a-42a2-cf6e";
+            BlockIO blockio = new BlockIO(apiKey);
+
+            JSONAPI json = blockio.getCurrentPrices();
+
+            Assert.AreEqual("success", json.Status);
+        }
+
+        [TestMethod]
+        public void can_getTransactionsSent()
+        {
+            string apiKey = "bb7c-fc4a-42a2-cf6e";
+            BlockIO blockio = new BlockIO(apiKey);
+
+            JSONAPI json = blockio.getTransactions("sent");
+
+            Assert.AreEqual("success", json.Status);
+        }
+
+        [TestMethod]
+        public void can_getTransactionsReceived()
+        {
+            string apiKey = "bb7c-fc4a-42a2-cf6e";
+            BlockIO blockio = new BlockIO(apiKey);
+
+            JSONAPI json = blockio.getTransactions("received");
+
+            Assert.AreEqual("success", json.Status);
+        }
+    }
+}
---
+++ ControllerTest/CoinBaseAPITest.cs
@@ -0,0 +1,61 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Coinbase;
+using Flurl.Http;
+using Flurl.Http.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoinBaseAPITesting
+{
+    [TestClass]
+    public class CoinBaseAPITest
+    {
+        private HttpTest server;
+
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            server = new HttpTest();
+        }
+
+        [TestCleanup]
+        public void AfterEachTest()
+        {
+            server.Dispose();
+        }
+
+        [TestMethod]
+        public async Task can_create_authorization_url()
+        {
+            var opts = new AuthorizeOptions
+            {
+                ClientId = "YOUR_CLIENT_ID",
+                RedirectUri = "YOUR_REDIRECT_URL",
+                State = "SECURE_RANDOM",
+                Scope = "wallet:accounts:read"
+            };
+            var authUrl = OAuthHelper.GetAuthorizeUrl(opts);
+
+            var url =
+                "https://www.coinbase.com/oauth/authorize?response_type=code&client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URL&state=SECURE_RANDOM&scope=wallet%3Aaccounts%3Aread";
+
+
+            var response = await authUrl.GetAsync();
+
+            server.ShouldHaveCalled(url);
+
+        }
+
+        [TestMethod]
+        public async Task can_revoke_token()
+        {
+            server.RespondWith("");
+
+            var x = await OAuthHelper.RevokeTokenAsync("fff", "vvv");
+
+            server.ShouldHaveCalled("https://api.coinbase.com/oauth/revoke")
+                .WithVerb(HttpMethod.Post)
+                .WithRequestBody("token=fff&access_token=vvv");
+        }
+    }
+}
